7af813dc7d3b70698251c838ced45c0f5d2c7dc7
Lautaro Avaro <lautaroavaro1@gmail.com>
2025-10-01 21:13:36 -0300

    agrego cors

--- src/main/kotlin/Security.kt
+++ src/main/kotlin/Security.kt
@@ -1,39 +1,12 @@
 package com.example
 
-import io.ktor.http.*
+
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.cors.routing.*
+
 
 fun Application.configureSecurity() {
-
-    // CORS
-    install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Get)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Patch)
-        allowMethod(HttpMethod.Delete)
-
-        allowHeader(HttpHeaders.ContentType)
-        allowHeader(HttpHeaders.Authorization)    // <-- necesario para Bearer tokens
-        allowCredentials = true
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("X-User-Id") // tu header personalizado
-
-
-
-        // Origenes permitidos
-        anyHost() // ⚠️ Solo para desarrollo, en producción limitar a tu frontend real
-        // Dev: permití tu front
-        allowHost("localhost:8080", schemes = listOf("http"))
-
-        allowNonSimpleContentTypes = true
-    }
-
-    // JWT Auth
     install(Authentication) {
         jwt("auth-jwt") {
             realm = JwtConfig.realm
@@ -45,9 +18,11 @@ fun Application.configureSecurity() {
                     .build()
             )
             validate { credential ->
-                // Aceptamos si trae un "sub" (userId) válido
-                credential.payload.getClaim("sub")?.asInt()?.let { JWTPrincipal(credential.payload) }
+                credential.payload.getClaim("sub")?.asInt()?.let {
+                    JWTPrincipal(credential.payload)
+                }
             }
         }
     }
 }
+
